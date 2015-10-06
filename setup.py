69693c11d9a3a6f1eb6b4fa5115a59bcf745aa2b
annapavt <annapavt@gmail.com>
2015-10-06 13:27:51 +0300

    update pypi config to release only on tagged commits

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='aria-tosca',
-    version='0.1',
+    version='0.2',
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
     packages=['aria_cli',
