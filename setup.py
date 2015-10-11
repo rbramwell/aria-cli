83ff669c390ddab9872b1aa906905006ab920bc8
annapavt <annapavt@gmail.com>
2015-10-11 15:47:29 +0300

    update version 0.1.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='aria-tosca',
-    version='0.1.2',
+    version='0.1.4',
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
     packages=['aria_cli',
