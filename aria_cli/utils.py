7b54772414722e6558f248860ffa24f3831030d1
Denis Makogon <lildee1991@gmail.com>
2016-02-11 11:51:08 +0200

    Apply appropriate Apache2 license header

--- aria_cli/utils.py
+++ aria_cli/utils.py
@@ -1,19 +1,18 @@
-# #######
-# Copyright (c) 2014 GigaSpaces Technologies Ltd. All rights reserved
+# Copyright (c) 2016 GigaSpaces Technologies Ltd. All rights reserved
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-############
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
+import contextlib
 import json
 import os
 import pkgutil
@@ -23,7 +22,7 @@ import getpass
 import yaml
 import pkg_resources
 
-from contextlib import contextmanager
+
 from jinja2 import environment
 
 
@@ -214,7 +213,7 @@ def get_import_resolver():
     return dsl_parser_utils.create_import_resolver(local_import_resolver)
 
 
-@contextmanager
+@contextlib.contextmanager
 def update_wd_settings():
     cosmo_wd_settings = load_aria_working_dir_settings()
     yield cosmo_wd_settings
